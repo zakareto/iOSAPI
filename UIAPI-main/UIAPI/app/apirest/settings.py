1e66ed68ec33996a451805c634b776938c42e4ff
zakareto <rafael.alejandro.2000@gmail.com>
2022-06-09 00:29:09 -0500

    Update settings.py

--- UIAPI-main/UIAPI/app/apirest/settings.py
+++ UIAPI-main/UIAPI/app/apirest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x68+w!+!lyila9fhu380w)q&x!szim06ki)-+$7rg&c07y5n%0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.93.248.32']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.93.248.32', '143.198.60.206']
 
 
 # Application definition
